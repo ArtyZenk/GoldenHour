30b889c33713ae844c12fbd94a0695d8f9042420
Artyom  Guzenko <artyomka13@gmail.com>
2024-03-11 20:30:26 +0300

    Initial Commit

---
+++ GoldenHour/GoldenHourApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoldenHourApp.swift
+//  GoldenHour
+//
+//  Created by Artyom Guzenko on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct GoldenHourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
